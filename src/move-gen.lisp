4e8491dbfe09545ad42ee7db4fac0f44143bfa04
Lihui Zhang <zlihui486@gmail.com>
2026-03-07 13:28:19 -0500

    Add move generation for sliding pieces

--- src/move-gen.lisp
+++ src/move-gen.lisp
@@ -22,3 +22,47 @@
 			 (make-capture square to piece target)
 			 (make-quiet square to piece)))))))
 	    offsets)))
+
+(defun ray-moves (state square piece color directions)
+  "Walk in each direction until blocked"
+  (mapcan (lambda (direction)
+	    (ray-in-direction state square piece color direction))
+	  directions))
+
+(defun ray-in-direction (state square piece color direction)
+  (let ((dr (car direction))
+	(df (cdr direction)))
+    (iterate
+      (with current-rank = (square-rank square))
+      (with current-file = (square-file square))
+      (for next-rank = (+ current-rank dr))
+      (for next-file = (+ current-file df))
+      (for next-square = (+ (* next-rank 8) next-file))
+      (while (and (<= 0 next-rank 7) (<= 0 next-file 7)))
+      (for target = (piece-at state next-square))
+
+      (setf current-rank next-rank
+	    current-file next-file)
+
+      (cond
+	((= target +empty+)
+	 (collect (make-quiet square next-square piece)))
+	((= (piece-color target) (enemy-of color))
+	 (collect (make-capture square next-square piece target))
+	 (finish))
+	(t (finish))))))
+
+(defun rook-moves (state color)
+  (let ((rooks (bb-squares (if (= color +white+) (white-rooks state) (black-rooks state))))
+	(rook-directions '((1 . 0) (-1 . 0) (0 . 1) (0 . -1))))
+    (mapcan (lambda (square) (ray-moves state square (piece-at state square) color rook-directions)) rooks)))
+
+(defun bishop-moves (state color)
+  (let ((bishop (bb-squares (if (= color +white+) (white-bishops state) (black-bishops state))))
+	(bishop-directions '((1 . 1) (1 . -1) (-1 . 1) (-1 . -1))))
+    (mapcan (lambda (square) (ray-moves state square (piece-at state square) color bishop-directions)) bishop)))
+
+(defun queen-moves (state color)
+  (let ((queen (bb-squares (if (= color +white+) (white-queens state) (black-queens state))))
+	(queen-directions '((1 . 1) (1 . -1) (-1 . 1) (-1 . -1) (1 . 0) (-1 . 0) (0 . 1) (0 . -1))))
+    (mapcan (lambda (square) (ray-moves state square (piece-at state square) color queen-directions)) queen)))
